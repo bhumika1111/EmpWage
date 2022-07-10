be953f66172e9992e0864a3f548e241e1f23e864
bhumika1111 <bhoyarpatil@434>
2022-07-10 20:58:34 +0530

    Added EmpWage

--- Program.cs
+++ Program.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 
-namespace UC3AddPartTimeEmpWage
+namespace UC4UsingCaseStatement
 {
     class Program
     {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
         static void Main(string[] args)
         {
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
-            int EMP_RATE_PER_HRS = 20;
+
 
             int EmpHrs = 0;
             int EmpWage = 0;
@@ -16,21 +17,24 @@ namespace UC3AddPartTimeEmpWage
             Random random = new Random();
 
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_PART_TIME)
-            {
+            switch (empCheck)
 
-                EmpHrs = 4;
-            }
-            else if (empCheck == IS_FULL_TIME)
             {
-                EmpHrs = 8;
-            }
-            else
-            {
-                EmpHrs = 0;
+                case IS_PART_TIME:
+                    EmpHrs = 4;
+                    break;
+
+                case IS_FULL_TIME:
+                    EmpHrs = 8;
+                    break;
+                default:
+
+
+                    EmpHrs = 0;
+                    break;
             }
 
-            EmpWage = EmpHrs * EMP_RATE_PER_HRS;
+            EmpWage = EmpHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp Wage:" + EmpWage);
         }
     }
