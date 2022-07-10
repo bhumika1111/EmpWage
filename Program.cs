39af0f936244fd6561cfb43d23ac1c01412d172e
bhumika1111 <bhoyarpatil@434>
2022-07-10 20:46:54 +0530

    Added EmpWage

--- Program.cs
+++ Program.cs
@@ -1,24 +1,31 @@
 ﻿using System;
 
-namespace UC1EmployeeIsPresentOrAbsent
+namespace UC2DailyEmpWage
 {
     class Program
     {
         static void Main(string[] args)
         {
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HRS = 20;
+
+            int empHrs = 0;
+            int empWage = 0;
+
             Random random = new Random();
 
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
             {
+                empHrs = 8;
 
-                Console.WriteLine("Employee is Present");
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HRS;
+            Console.WriteLine("Emp Wage:" + empWage);
         }
     }
 }
